4e0f8f738fe96df24549b69ad0c47d36cd0ca9a7
DALLIN11soccer <noodletrooper6@gmail.com>
2025-01-16 19:35:53 -0700

    Implemented basic ChessPosition class

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Objects;
+
 /**
  * Represents a single square position on a chess board
  * <p>
@@ -7,8 +9,25 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessPosition {
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPosition that = (ChessPosition) o;
+        return Row == that.Row && Col == that.Col;
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(Row, Col);
+    }
+
+    private int Row;
+    private int Col;
     public ChessPosition(int row, int col) {
+        this.Row = row;
+        this.Col = col;
     }
 
     /**
@@ -16,7 +35,7 @@ public class ChessPosition {
      * 1 codes for the bottom row
      */
     public int getRow() {
-        throw new RuntimeException("Not implemented");
+        return this.Row;
     }
 
     /**
@@ -24,6 +43,6 @@ public class ChessPosition {
      * 1 codes for the left row
      */
     public int getColumn() {
-        throw new RuntimeException("Not implemented");
+        return this.Col;
     }
 }
