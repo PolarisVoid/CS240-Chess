08dffbe6fff317f1b19dbf0af2fb478559ab6249
DALLIN11soccer <noodletrooper6@gmail.com>
2025-01-16 19:35:06 -0700

    Added implementations for the basics of ChessBoard

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -7,9 +10,47 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessBoard {
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessBoard that = (ChessBoard) o;
+        return Objects.deepEquals(Board, that.Board);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(Board);
+    }
+
+    @Override
+    public String toString() {
+        String boardString = "";
+        for (int i = 0; i < 8; i++) {
+            boardString = boardString + " {";
+            for (int j = 0; j < 8; j++) {
+                boardString = boardString + " " + Board[i][j];
+            }
+            boardString = boardString + " }\n";
+        }
+
+
+        return "ChessBoard{\n" +
+                "Board=" + boardString +
+                '}';
+    }
+
+    private ChessPiece[][] Board;
 
     public ChessBoard() {
-        
+        this.Board = new ChessPiece[8][8];
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                this.Board[i][j] = null;
+            }
+        }
+        this.resetBoard();
     }
 
     /**
@@ -19,7 +60,11 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        if (1 > position.getRow() || 8 < position.getRow() || 1 > position.getColumn() || 8 < position.getColumn()){
+            return;
+        }
+
+        this.Board[8-position.getRow()][position.getColumn()-1] = piece;
     }
 
     /**
@@ -30,7 +75,22 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        if (1 > position.getRow() || 8 < position.getRow() || 1 > position.getColumn() || 8 < position.getColumn()){
+            return null;
+        }
+        return this.Board[8-position.getRow()][position.getColumn()-1];
+    }
+
+    private void SetupRow(int row, ChessPiece.PieceType[] Pieces, ChessGame.TeamColor color) {
+        for (int i = 0; i < 8; ++i) {
+            this.Board[row][i] = new ChessPiece(color, Pieces[i]);
+        }
+    }
+
+    private void SetupEmptyRow(int row) {
+        for (int i = 0; i < 8; ++i) {
+            this.Board[row][i] = null;
+        }
     }
 
     /**
@@ -38,6 +98,34 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        ChessPiece.PieceType[] MainRow = {
+            ChessPiece.PieceType.ROOK,
+            ChessPiece.PieceType.KNIGHT,
+            ChessPiece.PieceType.BISHOP,
+            ChessPiece.PieceType.QUEEN,
+            ChessPiece.PieceType.KING,
+            ChessPiece.PieceType.BISHOP,
+            ChessPiece.PieceType.KNIGHT,
+            ChessPiece.PieceType.ROOK
+        };
+        ChessPiece.PieceType[] PawnRow = {
+            ChessPiece.PieceType.PAWN,
+            ChessPiece.PieceType.PAWN,
+            ChessPiece.PieceType.PAWN,
+            ChessPiece.PieceType.PAWN,
+            ChessPiece.PieceType.PAWN,
+            ChessPiece.PieceType.PAWN,
+            ChessPiece.PieceType.PAWN,
+            ChessPiece.PieceType.PAWN,
+        };
+
+        this.SetupRow(0, MainRow, ChessGame.TeamColor.BLACK);
+        this.SetupRow(1, PawnRow, ChessGame.TeamColor.BLACK);
+        this.SetupEmptyRow(2);
+        this.SetupEmptyRow(3);
+        this.SetupEmptyRow(4);
+        this.SetupEmptyRow(5);
+        this.SetupRow(6, PawnRow, ChessGame.TeamColor.WHITE);
+        this.SetupRow(7, MainRow, ChessGame.TeamColor.WHITE);
     }
 }
