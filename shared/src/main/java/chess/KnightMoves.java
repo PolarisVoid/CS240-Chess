de2a063b6fcc9fdb2ea809b9bee32964674e493f
DALLIN11soccer <noodletrooper6@gmail.com>
2025-01-22 20:37:02 -0700

    Finished Knight Moves

--- shared/src/main/java/chess/KnightMoves.java
+++ shared/src/main/java/chess/KnightMoves.java
@@ -1,9 +1,35 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class KnightMoves {
     public Collection<ChessMove> getMoves(ChessBoard board, ChessPosition myPosition, ChessGame.TeamColor color) {
-        return null;
+        int[][] Offsets = {
+                {2, 1}, {2, -1},
+                {-2, 1}, {-2, -1},
+                {1, 2}, {1, -2},
+                {-1, 2}, {-1, -2}
+        };
+
+        Collection<ChessMove> moves = new ArrayList<ChessMove>();
+        int row = myPosition.getRow();
+        int col = myPosition.getColumn();
+        for (int[] Offset : Offsets) {
+            int newRow = row + Offset[0];
+            int newCol = col + Offset[1];
+            if (newRow < 1 || newRow > 8 || newCol < 1 || newCol > 8) {
+                continue;
+            }
+            ChessPiece currentSquare = board.getPiece(new ChessPosition(newRow, newCol));
+            if (currentSquare != null) {
+                if (currentSquare.getTeamColor() != color) {
+                    moves.add(new ChessMove(new ChessPosition(myPosition.getRow(), myPosition.getColumn()), new ChessPosition(newRow, newCol), null));
+                }
+                continue;
+            }
+            moves.add(new ChessMove(new ChessPosition(myPosition.getRow(), myPosition.getColumn()), new ChessPosition(newRow, newCol), null));
+        }
+        return moves;
     }
 }
