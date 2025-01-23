3a3e20cc4a53e0a872dc637af252e2523458440a
DALLIN11soccer <noodletrooper6@gmail.com>
2025-01-22 20:05:33 -0700

    Finished Rook Moves

--- shared/src/main/java/chess/RookMoves.java
+++ shared/src/main/java/chess/RookMoves.java
@@ -1,78 +1,16 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
 
 public class RookMoves {
     public Collection<ChessMove> getMoves(ChessBoard board, ChessPosition myPosition, ChessGame.TeamColor color) {
-        Collection<ChessMove> moves = new Collection<ChessMove>() {
-            @Override
-            public int size() {
-                return 0;
-            }
+        Collection<ChessMove> moves = new ArrayList<ChessMove>();
 
-            @Override
-            public boolean isEmpty() {
-                return false;
-            }
-
-            @Override
-            public boolean contains(Object o) {
-                return false;
-            }
-
-            @Override
-            public Iterator<ChessMove> iterator() {
-                return null;
-            }
-
-            @Override
-            public Object[] toArray() {
-                return new Object[0];
-            }
-
-            @Override
-            public <T> T[] toArray(T[] a) {
-                return null;
-            }
-
-            @Override
-            public boolean add(ChessMove chessMove) {
-                return false;
-            }
-
-            @Override
-            public boolean remove(Object o) {
-                return false;
-            }
-
-            @Override
-            public boolean containsAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public boolean addAll(Collection<? extends ChessMove> c) {
-                return false;
-            }
-
-            @Override
-            public boolean removeAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public boolean retainAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public void clear() {
-
-            }
-        };
-        int row = myPosition.getRow();
+        // Left
+        int row = myPosition.getRow() - 1;
         while (row > 0) {
+//            System.out.print("Left " + row);
             ChessPiece currentSquare = board.getPiece(new ChessPosition(row, myPosition.getColumn()));
             if (currentSquare != null) {
                 if (currentSquare.getTeamColor() != color) {
@@ -83,8 +21,11 @@ public class RookMoves {
             moves.add(new ChessMove(new ChessPosition(myPosition.getRow(), myPosition.getColumn()), new ChessPosition(row, myPosition.getColumn()), null));
             row--;
         }
-        row = myPosition.getRow();
-        while (row < 8) {
+
+        // Right
+        row = myPosition.getRow() + 1;
+        while (row < 9) {
+//            System.out.print("Right " + row);
             ChessPiece currentSquare = board.getPiece(new ChessPosition(row, myPosition.getColumn()));
             if (currentSquare != null) {
                 if (currentSquare.getTeamColor() != color) {
@@ -95,10 +36,14 @@ public class RookMoves {
             moves.add(new ChessMove(new ChessPosition(myPosition.getRow(), myPosition.getColumn()), new ChessPosition(row, myPosition.getColumn()), null));
             row++;
         }
-        int col = myPosition.getColumn();
+
+        // Down
+        int col = myPosition.getColumn() - 1;
         while (col > 0) {
+//            System.out.print("Down" + col);
             ChessPiece currentSquare = board.getPiece(new ChessPosition(myPosition.getRow(), col));
             if (currentSquare != null) {
+                System.out.print(currentSquare);
                 if (currentSquare.getTeamColor() != color) {
                     moves.add(new ChessMove(new ChessPosition(myPosition.getRow(), myPosition.getColumn()), new ChessPosition(myPosition.getRow(), col), null));
                 }
@@ -107,8 +52,11 @@ public class RookMoves {
             moves.add(new ChessMove(new ChessPosition(myPosition.getRow(), myPosition.getColumn()), new ChessPosition(myPosition.getRow(), col), null));
             col--;
         }
-        col = myPosition.getColumn();
-        while (col < 8) {
+
+        // Up
+        col = myPosition.getColumn() + 1;
+        while (col < 9) {
+//            System.out.print("Up" + col);
             ChessPiece currentSquare = board.getPiece(new ChessPosition(myPosition.getRow(), col));
             if (currentSquare != null) {
                 if (currentSquare.getTeamColor() != color) {
