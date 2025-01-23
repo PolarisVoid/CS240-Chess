972838f2211eb3cf16b8542c968c69d177154ec4
DALLIN11soccer <noodletrooper6@gmail.com>
2025-01-22 20:05:20 -0700

    Finished Bishop Moves.

--- shared/src/main/java/chess/BishopMoves.java
+++ shared/src/main/java/chess/BishopMoves.java
@@ -1,9 +1,72 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class BishopMoves {
     public Collection<ChessMove> getMoves(ChessBoard board, ChessPosition myPosition, ChessGame.TeamColor color) {
-        return null;
+        Collection<ChessMove> moves = new ArrayList<ChessMove>();
+
+        int row = myPosition.getRow() - 1;
+        int col = myPosition.getColumn() - 1;
+        while (row > 0 && col > 0) {
+            ChessPiece currentSquare = board.getPiece(new ChessPosition(row, col));
+            if (currentSquare != null) {
+                if (currentSquare.getTeamColor() != color) {
+                    moves.add(new ChessMove(new ChessPosition(myPosition.getRow(), myPosition.getColumn()), new ChessPosition(row, col), null));
+                }
+                break;
+            }
+            moves.add(new ChessMove(new ChessPosition(myPosition.getRow(), myPosition.getColumn()), new ChessPosition(row, col), null));
+            row--;
+            col--;
+        }
+
+        row = myPosition.getRow() - 1;
+        col = myPosition.getColumn() + 1;
+        while (row > 0 && col < 9) {
+            ChessPiece currentSquare = board.getPiece(new ChessPosition(row, col));
+            if (currentSquare != null) {
+                if (currentSquare.getTeamColor() != color) {
+                    moves.add(new ChessMove(new ChessPosition(myPosition.getRow(), myPosition.getColumn()), new ChessPosition(row, col), null));
+                }
+                break;
+            }
+            moves.add(new ChessMove(new ChessPosition(myPosition.getRow(), myPosition.getColumn()), new ChessPosition(row, col), null));
+            row--;
+            col++;
+        }
+
+        row = myPosition.getRow() + 1;
+        col = myPosition.getColumn() - 1;
+        while (row < 9 && col > 0) {
+            ChessPiece currentSquare = board.getPiece(new ChessPosition(row, col));
+            if (currentSquare != null) {
+                if (currentSquare.getTeamColor() != color) {
+                    moves.add(new ChessMove(new ChessPosition(myPosition.getRow(), myPosition.getColumn()), new ChessPosition(row, col), null));
+                }
+                break;
+            }
+            moves.add(new ChessMove(new ChessPosition(myPosition.getRow(), myPosition.getColumn()), new ChessPosition(row, col), null));
+            row++;
+            col--;
+        }
+
+        row = myPosition.getRow() + 1;
+        col = myPosition.getColumn() + 1;
+        while (row < 9 && col < 9) {
+            ChessPiece currentSquare = board.getPiece(new ChessPosition(row, col));
+            if (currentSquare != null) {
+                if (currentSquare.getTeamColor() != color) {
+                    moves.add(new ChessMove(new ChessPosition(myPosition.getRow(), myPosition.getColumn()), new ChessPosition(row, col), null));
+                }
+                break;
+            }
+            moves.add(new ChessMove(new ChessPosition(myPosition.getRow(), myPosition.getColumn()), new ChessPosition(row, col), null));
+            row++;
+            col++;
+        }
+
+        return moves;
     }
 }
