fc8475739dd60a6442e5311b91aad9178c60ad4d
DALLIN11soccer <noodletrooper6@gmail.com>
2025-01-22 20:05:45 -0700

    Finished Queen Moves

--- shared/src/main/java/chess/QueenMoves.java
+++ shared/src/main/java/chess/QueenMoves.java
@@ -1,9 +1,15 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class QueenMoves {
     public Collection<ChessMove> getMoves(ChessBoard board, ChessPosition myPosition, ChessGame.TeamColor color) {
-        return null;
+        Collection<ChessMove> moves = new ArrayList<ChessMove>();
+
+        moves.addAll(new RookMoves().getMoves(board, myPosition, color));
+        moves.addAll(new BishopMoves().getMoves(board, myPosition, color));
+
+        return moves;
     }
 }
