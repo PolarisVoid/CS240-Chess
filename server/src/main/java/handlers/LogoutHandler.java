b7c8fddd3b545ca0ed8f4b76bb42fd3c1f0334f6
DALLIN11soccer <noodletrooper6@gmail.com>
2025-02-26 18:44:50 -0700

    Added the handling portion of Logout

--- server/src/main/java/handlers/LogoutHandler.java
+++ server/src/main/java/handlers/LogoutHandler.java
@@ -1,13 +1,35 @@
 package handlers;
 
+import exceptions.InvalidRequestException;
+import exceptions.UnathorizedException;
+import requests.LogoutRequest;
+import services.LogoutService;
 import spark.Request;
 import spark.Response;
 import spark.Route;
 
 public class LogoutHandler implements Route {
 
+    private LogoutRequest createRequest(Request request) throws InvalidRequestException {
+        try {
+            String authToken = request.attribute("authorization");
+            return new LogoutRequest(authToken);
+        } catch (Exception e) {
+            throw new InvalidRequestException(e.toString());
+        }
+    }
+
     @Override
-    public Object handle(Request request, Response response) throws Exception {
-        return null;
+    public Object handle(Request request, Response response) {
+        try {
+            LogoutService.logout(createRequest(request));
+            return "{}";
+        } catch (UnathorizedException e) {
+            response.status(401);
+            return "{\"message\": \"Error: unauthorized\" }";
+        } catch (Exception e) {
+            response.status(500);
+            return "{\"message\": \"Error: " + e + "\" }";
+        }
     }
 }
