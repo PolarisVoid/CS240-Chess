6eddc2930312bb289bd0bacb9e481edb61d5e8dd
DALLIN11soccer <noodletrooper6@gmail.com>
2025-02-26 20:13:38 -0700

    Added logic for Clear Database Service

--- server/src/main/java/services/ClearDatabaseService.java
+++ server/src/main/java/services/ClearDatabaseService.java
@@ -1,8 +1,15 @@
 package services;
 
+import dataaccess.DataAccessException;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryGameDAO;
+import dataaccess.MemoryUserDAO;
+
 public class ClearDatabaseService {
 
-    public static void clearDatabase() {
-        return;
+    public static void clearDatabase() throws DataAccessException {
+        new MemoryAuthDAO().clear();
+        new MemoryGameDAO().clear();
+        new MemoryUserDAO().clear();
     }
 }
