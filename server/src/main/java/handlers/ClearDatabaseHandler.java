622bf0ae58a4bb5afcac7fe36f62fb9237249cb9
DALLIN11soccer <noodletrooper6@gmail.com>
2025-02-26 19:50:41 -0700

    Added the handling portion of Clear Database

--- server/src/main/java/handlers/ClearDatabaseHandler.java
+++ server/src/main/java/handlers/ClearDatabaseHandler.java
@@ -1,5 +1,6 @@
 package handlers;
 
+import services.ClearDatabaseService;
 import spark.Request;
 import spark.Response;
 import spark.Route;
@@ -7,7 +8,13 @@ import spark.Route;
 public class ClearDatabaseHandler implements Route {
 
     @Override
-    public Object handle(Request request, Response response) throws Exception {
-        return null;
+    public Object handle(Request request, Response response) {
+        try {
+            ClearDatabaseService.clearDatabase();
+            return "{}";
+        } catch (Exception e) {
+            response.status(500);
+            return "{\"message\": \"Error: " + e.toString() + "\" }";
+        }
     }
 }
--- server/src/main/java/services/ClearDatabaseService.java
+++ server/src/main/java/services/ClearDatabaseService.java
@@ -1,4 +1,8 @@
 package services;
 
 public class ClearDatabaseService {
+
+    public static void clearDatabase() {
+        return;
+    }
 }
