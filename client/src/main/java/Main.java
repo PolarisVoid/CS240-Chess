a42279ed32384652959bd8d2fea04219270a2f69
DALLIN11soccer <noodletrooper6@gmail.com>
2025-03-19 17:19:53 -0600

    Started initialization of client and server on start up

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,10 @@
-import chess.*;
+import server.Server;
+import ui.Client;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        Server server = new Server();
+        var port = server.run(0);
+        new Client();
     }
 }
