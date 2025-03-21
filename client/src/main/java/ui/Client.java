fc82acc7c5cd769fe368140399d2749f1d077f69
DALLIN11soccer <noodletrooper6@gmail.com>
2025-03-20 19:34:02 -0600

    Initial layout of Command Processing.

--- client/src/main/java/ui/Client.java
+++ client/src/main/java/ui/Client.java
@@ -2,7 +2,40 @@ package ui;
 
 public class Client {
 
+    private boolean loggedIn;
     public Client() {
+        String command = "";
+        while (!command.equals("Quit")) {
+            if (!loggedIn) {
+                command = PreLoginUI();
+            } else {
+                command = PostLoginUI();
+            }
 
+            ProcessCommand(command);
+        }
+    }
+
+    private void ProcessCommand(String command) {
+        switch (command) {
+            case "Help"         -> {}
+            case "Quit"         -> {}
+            case "Login"        -> {}
+            case "Register"     -> {}
+            case "Logout"       -> {}
+            case "Create"       -> {}
+            case "List"         -> {}
+            case "Join"         -> {}
+            case "Observe"      -> {}
+            case null, default  -> {}
+        }
+    }
+
+    private String PreLoginUI() {
+        return "";
+    }
+
+    private String PostLoginUI() {
+        return "";
     }
 }
