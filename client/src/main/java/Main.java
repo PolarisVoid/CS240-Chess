1ae1b88c65929ea9c842699a9ef1eba9809d6cbb
DALLIN11soccer <noodletrooper6@gmail.com>
2025-03-24 18:51:37 -0600

    Updated how the server starts, serverFacade gets passed in, and how the Client gets called.

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,10 +1,12 @@
 import server.Server;
 import ui.Client;
+import ui.ServerFacade;
 
 public class Main {
     public static void main(String[] args) {
         Server server = new Server();
-        var port = server.run(0);
-        new Client();
+        var port = server.run(8080);
+        new Client(new ServerFacade(port));
+        server.stop();
     }
 }
