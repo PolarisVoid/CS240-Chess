f388722c3bf66990140fa2b73bce360395ef1142
DALLIN11soccer <noodletrooper6@gmail.com>
2025-03-24 18:52:15 -0600

    Updated Server Facade to have the beginnings of all the calls.

--- client/src/main/java/ui/ServerFacade.java
+++ client/src/main/java/ui/ServerFacade.java
@@ -1,4 +1,52 @@
 package ui;
 
+import model.AuthData;
+import model.GameData;
+
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.util.Collection;
+
 public class ServerFacade {
+
+    private String serverURL;
+    public ServerFacade(int port) {
+        serverURL = "http://localhost:" + port;
+    }
+
+    public AuthData Login() {
+        String url = serverURL + "/login";
+        try {
+            URI uri = new URI(url);
+            HttpURLConnection http = (HttpURLConnection) uri.toURL().openConnection();
+            http.setRequestMethod("POST");
+        } catch (Exception e) {
+
+        }
+        return null;
+    }
+
+    public AuthData Register() {
+        return null;
+    }
+
+    public void Logout() {
+
+    }
+
+    public int CreateGame() {
+        return 0;
+    }
+
+    public Collection<GameData> ListGames() {
+        return null;
+    }
+
+    public void JoinGame() {
+
+    }
+
+    public void ObserveGame() {
+
+    }
 }
