4489e6021b7a7d628e490915c26f19799ab2caae
DALLIN11soccer <noodletrooper6@gmail.com>
2025-04-09 16:58:08 -0600

    Initial Pre Login Interface

---
+++ client/src/main/java/interfaces/PreLoginInterface.java
@@ -0,0 +1,75 @@
+package interfaces;
+
+import exception.AlreadyTaken;
+import exception.BadRequest;
+import exception.Unauthorized;
+import model.AuthData;
+import ui.Client;
+
+import java.util.Arrays;
+
+public class PreLoginInterface extends Interface {
+    private static final String[] commands = {"Help", "Quit", "Login", "Register"};
+
+    public PreLoginInterface(Client client) {
+        super(client);
+    }
+
+    public String ui() {
+        while (true) {
+            String command = promptString("");
+            if (Arrays.asList(commands).contains(command)) {
+                return command;
+            }
+            System.out.println("Invalid Command");
+        }
+    }
+
+    public void help() {
+        System.out.println("Help - Displays commands the user can run");
+        System.out.println("Quit - Exits the program");
+        System.out.println("Login - Allows for signing in");
+        System.out.println("Register - Allows for creating a new account");
+    }
+
+    public void login() {
+        String username = promptString("Enter Your Username:");
+        String password = promptString("Enter Your Password:");
+
+        try {
+            AuthData authData = serverFacade.login(username, password);
+            client.setAuthToken(authData.authToken());
+        } catch (Unauthorized e) {
+            System.out.println("Invalid Login");
+            return;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+
+        client.setInterface(new PostLoginInterface(client));
+    }
+
+    public void register() {
+        String username = promptString("Enter Your Desired Username:");
+        String password = promptString("Enter Your Desired Password:");
+        String email = promptString("Enter Your Email Address:");
+
+        try {
+            AuthData authData = serverFacade.register(username, password, email);
+            client.setAuthToken(authData.authToken());
+        } catch (AlreadyTaken e) {
+            System.out.println("Username Already Taken.");
+            return;
+        } catch (BadRequest e) {
+            System.out.println("Bad User Input. Please fill in all fields with valid values.");
+            return;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+
+        client.setInterface(new PostLoginInterface(client));
+    }
+
+}
